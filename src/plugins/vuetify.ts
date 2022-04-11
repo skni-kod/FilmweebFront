baedaed2d2f22c431f91ae8c1715c4d375856dc4
Hubert Futoma <futomahubert@gmail.com>
2022-04-11 22:30:39 +0200

    Vuetify Colors

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#FFF', //do zmiany
+                secondary: '#000', //do zmiany
+                accent: '#f7f7f7',
+            },
+        },
+    },
 });
