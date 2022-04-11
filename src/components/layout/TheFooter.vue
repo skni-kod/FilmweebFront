b007012ec3ce85f75d5e11d3122c9c81e376e7b9
Hubert Futoma <futomahubert@gmail.com>
2022-04-11 22:28:04 +0200

    Basic Configuration
    -wydzielenie home, navbar, footer
    -usunięcie zbędnych plików wygenerowanych wraz ze stworzeniem projektu

---
+++ src/components/layout/TheFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Tutaj będzie stopka</h1>
+</template>
+
+<style scoped>
+h1 {
+  position: absolute;
+  top: 90%;
+  left: 20%;
+}
+</style>
---
+++ src/views/TheHome.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Tutaj będzie nasza strona główna</h1>
+</template>
+
+<style scoped>
+h1 {
+  position: absolute;
+  top: 50%;
+  left: 40%;
+}
+</style>
