f1e8e3920d5598d0f81a943dcea54b4e3cff1895
MaciekFerenc <maciekferenc1@gmail.com>
2022-04-15 16:09:25 +0200

    Review list
    Added user reviews display, edit and delete buttons are not functional for now.
    TODO
    fix profile sidebar floating over main nav bar when scrolling down

---
+++ src/components/profilePage/ReviewListItem.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class ReviewListItem extends Vue {
+  @Prop({ required: true }) readonly review: any;
+}
+</script>
+
+<template>
+  <v-list-item>
+    <v-card outlined class="review">
+      <v-card-title class="text-h6">{{ review.movieTitle }}</v-card-title>
+      <v-card-subtitle> Data dodania: {{ review.date }} </v-card-subtitle>
+      <v-divider></v-divider>
+      <v-card-text>
+        <p>{{ review.content }}</p>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text @click=""> Edytuj </v-btn>
+        <v-btn text @click=""> Usuń </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-list-item>
+</template>
+
+<style scoped>
+.review {
+  width: 100%;
+}
+p {
+  text-align: justify;
+}
+</style>
--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -5,18 +5,22 @@ import { Component } from "vue-property-decorator";
 import ProfileSidebar from "@/components/profilePage/ProfileSidebar.vue";
 import ProfileInfoForm from "@/components/profilePage/ProfileInfoForm.vue";
 import ChangePasswordForm from "@/components/profilePage/ChangePasswordForm.vue";
+import ReviewList from "@/components/profilePage/ReviewList.vue";
 
 @Component({
   components: {
     ProfileSidebar,
     ProfileInfoForm,
     ChangePasswordForm,
+    ReviewList,
   },
 })
 export default class UserProfile extends Vue {
   private sidebar: boolean = false;
+
+  // ----------------------------------------------
+  // example data
   public user: object = {
-    // example user
     firstName: "Jan",
     lastName: "Kowalski",
     nickName: "jankowalski123",
@@ -24,6 +28,28 @@ export default class UserProfile extends Vue {
     avatar:
       "https://play-lh.googleusercontent.com/5LIMaa7WTNy34bzdFhBETa2MRj7mFJZWb8gCn_uyxQkUvFx_uOFCeQjcK16c6WpBA3E=s180-rwa",
   };
+  public userReviews: Array<any> = [
+    {
+      movieTitle: "Tytul1",
+      date: "data1",
+      type: "pozytywna",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce volutpat hendrerit cursus. Morbi id mauris vehicula, commodo risus at, molestie ante. Curabitur gravida tellus at dui ultrices, eu pulvinar eros euismod. Nam laoreet orci sit amet cursus sollicitudin. Vestibulum vitae tellus accumsan, vulputate dolor vel, convallis augue. In mollis massa ac posuere dapibus. Morbi pharetra ante ut augue varius, et convallis risus pharetra. Vivamus ut nisl efficitur, tempus ante nec, luctus mi. Maecenas euismod malesuada arcu, vitae accumsan arcu pretium id. Proin sit amet turpis ex. Maecenas pharetra vehicula arcu quis dapibus.Integer leo purus, tincidunt sed vehicula id, hendrerit vel nisl. Sed laoreet at mauris non ullamcorper. Vestibulum sed scelerisque tellus, quis ultricies leo. Aenean tristique tincidunt lectus, vitae pretium nisi aliquet non. Quisque tempus scelerisque est nec pretium. Pellentesque viverra hendrerit dignissim. Vestibulum elit erat, porttitor eu tincidunt eu, auctor et orci. Integer ac tempor magna. Mauris imperdiet sed libero sit amet hendrerit. Sed dignissim porttitor ultricies. Morbi commodo, est a faucibus malesuada, libero tortor pharetra augue, at tempor odio dui at nunc. Nullam ut lacus volutpat nunc sodales commodo.Donec tincidunt tempus dolor, sed pretium mauris aliquam vitae. Curabitur tristique ex nec quam ullamcorper, sed dictum libero semper. Mauris ac neque finibus, aliquet diam quis, ullamcorper diam. Duis malesuada efficitur ligula at dignissim. Morbi a tortor dignissim, rutrum tortor non, scelerisque ante. Phasellus pulvinar dapibus scelerisque. Vivamus tincidunt sed ligula non volutpat. Proin tristique a mi sit amet ornare. Proin at mi lacus.",
+    },
+    {
+      movieTitle: "Tytul2",
+      date: "data2",
+      type: "negatywna",
+      content: "tresc2",
+    },
+    {
+      movieTitle: "Tytul3",
+      date: "data3",
+      type: "pozytywna",
+      content: "tresc3",
+    },
+  ];
+  // ----------------------------------------------
 
   navOptions: Array<any> = [
     {
@@ -68,6 +94,9 @@ export default class UserProfile extends Vue {
     <v-col></v-col>
     <v-col v-if="navOptions[0].show"> <ProfileInfoForm :user="user" /></v-col>
     <v-col v-else-if="navOptions[1].show"> <ChangePasswordForm /></v-col>
+    <v-col v-else-if="navOptions[2].show">
+      <ReviewList :reviews="userReviews"
+    /></v-col>
   </v-container>
 </template>
 
