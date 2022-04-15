dff67c6bd3cdd52bc8b0aa53b719eda9f7b25841
MaciekFerenc <maciekferenc1@gmail.com>
2022-04-15 16:09:25 +0200

    Review list
    Added user reviews display, edit and delete buttons are not functional for now.
    TODO
    fix profile sidebar floating over main nav bar when scrolling down

---
+++ src/components/profilePage/ReviewList.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import ReviewListItem from "@/components/profilePage/ReviewListItem.vue";
+
+@Component({
+  components: {
+    ReviewListItem,
+  },
+})
+export default class ReviewList extends Vue {
+  @Prop({ required: true }) readonly reviews: Array<any>;
+}
+</script>
+
+<template>
+  <v-container>
+    <v-list>
+      <reviewListItem
+        v-for="(reviewItem, i) in reviews"
+        :key="i"
+        :review="reviewItem"
+      />
+    </v-list>
+  </v-container>
+</template>
