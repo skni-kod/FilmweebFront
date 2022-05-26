e0bfc421535028c19f5181ed866fb9fc8eec0438
MaciekFerenc <maciekferenc1@gmail.com>
2022-05-26 14:07:33 +0200

    Add admin page

---
+++ src/components/profilePage/Admin.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card> Administrator </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class Admin extends Vue {}
+</script>
