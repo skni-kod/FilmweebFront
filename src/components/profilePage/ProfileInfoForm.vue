5ccb7eaaaccd2aaf2e6db77d5ad786c8064ede54
MaciekFerenc <maciekferenc1@gmail.com>
2022-05-26 14:09:47 +0200

    Code rework

--- src/components/profilePage/ProfileInfoForm.vue
+++ src/components/profilePage/ProfileInfoForm.vue
@@ -1,82 +1,92 @@
-<script lang="ts">
-import Vue from "vue";
-import { Component, Prop } from "vue-property-decorator";
-
-@Component
-export default class ProfileInfoForm extends Vue {
-  @Prop({ required: true }) readonly user: any;
-
-  public formData: Array<any> = [
-    {
-      label: "Imię",
-      value: this.user.firstName,
-      minLength: 3,
-      maxLength: 32,
-      rules: [
-        (v: string) => !!v || "Imię jest wymagane",
-        (v: string) =>
-          v.length >= 3 || "Imię powinno mieć przynajmniej 3 znaki",
-        (v: string) =>
-          v.length <= 32 || "Imię nie może mieć więcej niż 32 znaki",
-      ],
-    },
-    {
-      label: "Nazwisko",
-      value: this.user.lastName,
-      rules: [
-        (v: string) => !!v || "Nazwisko jest wymagane",
-        (v: string) =>
-          v.length >= 3 || "Nazwisko powinno mieć przynajmniej 3 znaki",
-        (v: string) =>
-          v.length <= 32 || "Nazwisko nie może mieć więcej niż 32 znaki",
-      ],
-    },
-    {
-      label: "Nazwa użytkowika",
-      value: this.user.nickName,
-      rules: [
-        (v: string) => !!v || "Nazwa użytkowika jest wymagane",
-        (v: string) =>
-          v.length >= 3 || "Nazwa użytkowika powinno mieć przynajmniej 3 znaki",
-        (v: string) =>
-          v.length <= 32 ||
-          "Nazwa użytkowika nie może mieć więcej niż 32 znaki",
-      ],
-    },
-    {
-      label: "Adres e-mail",
-      value: this.user.email,
-      rules: [
-        (v: string) => !!v || "Adres e-mail jest wymagany",
-        (v: string) =>
-          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,4})+$/.test(v) ||
-          "Nieprawidłowy adres e-mail",
-      ],
-    },
-  ];
-
-  submit(): void {
-    console.log("form submit");
-  }
-}
-</script>
-
 <template>
   <v-card>
     <v-form class="form">
       <h3>Dane użytkownika</h3>
       <v-text-field
         v-for="(input, i) in formData"
+        :key="i"
         v-model="input.value"
         :label="input.label"
         color="dark"
         required
         :rules="input.rules"
       ></v-text-field>
+      <v-file-input
+        label="Zmień awatar"
+        color="dark"
+        prepend-icon="mdi-camera"
+        accept="image/png, image/jpeg, image/bmp"
+      ></v-file-input>
       <v-btn type="submit" @click.prevent="submit"> Zapisz </v-btn>
     </v-form>
   </v-card>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class ProfileInfoForm extends Vue {
+  data() {
+    return {
+      formData: [
+        {
+          label: "Imię",
+          value: "Jan",
+          minLength: 3,
+          maxLength: 32,
+          rules: [
+            (v: string) => !!v || "Imię jest wymagane",
+            (v: string) =>
+              v.length >= 3 || "Imię powinno mieć przynajmniej 3 znaki",
+            (v: string) =>
+              v.length <= 32 || "Imię nie może mieć więcej niż 32 znaki",
+          ],
+        },
+        {
+          label: "Nazwisko",
+          value: "Kowalski",
+          rules: [
+            (v: string) => !!v || "Nazwisko jest wymagane",
+            (v: string) =>
+              v.length >= 3 || "Nazwisko powinno mieć przynajmniej 3 znaki",
+            (v: string) =>
+              v.length <= 32 || "Nazwisko nie może mieć więcej niż 32 znaki",
+          ],
+        },
+        {
+          label: "Nazwa użytkowika",
+          value: "jankowalski123",
+          rules: [
+            (v: string) => !!v || "Nazwa użytkowika jest wymagane",
+            (v: string) =>
+              v.length >= 3 ||
+              "Nazwa użytkowika powinna mieć przynajmniej 3 znaki",
+            (v: string) =>
+              v.length <= 32 ||
+              "Nazwa użytkowika nie może mieć więcej niż 32 znaki",
+          ],
+        },
+        {
+          label: "Adres e-mail",
+          value: "jankowalski@gmail.com",
+          rules: [
+            (v: string) => !!v || "Adres e-mail jest wymagany",
+            (v: string) =>
+              /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,4})+$/.test(v) ||
+              "Nieprawidłowy adres e-mail",
+          ],
+        },
+      ],
+    };
+  }
+
+  submit(): void {
+    console.log("form submit");
+  }
+}
+</script>
 
 <style scoped>
 .form {
