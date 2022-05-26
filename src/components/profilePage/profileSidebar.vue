31cc12bcc91ef06377f958d492fd19af9b3a7322
MaciekFerenc <maciekferenc1@gmail.com>
2022-05-26 14:13:17 +0200

    Code rework, profile navigation uses router now

--- src/components/profilePage/profileSidebar.vue
+++ src/components/profilePage/profileSidebar.vue
@@ -1,73 +1,93 @@
+<template>
+  <v-navigation-drawer
+    app
+    hide-overlay
+    v-model="sidebarShown"
+    :style="{ top: $vuetify.application.top + 'px', zIndex: 4 }"
+  >
+    <v-list>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            <v-list-item-avatar>
+              <v-img :src="user.avatar"></v-img>
+            </v-list-item-avatar>
+            {{ user.nickName }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list-item
+        v-for="(page, i) in pages"
+        :key="i"
+        link
+        :to="page.link"
+        replace
+      >
+        <v-list-item-icon>
+          <v-icon v-text="page.icon"></v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title v-text="page.name"></v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
 <script lang="ts">
 import Vue from "vue";
-import { Component, Prop } from "vue-property-decorator";
+import { Component } from "vue-property-decorator";
 
 @Component
 export default class ProfileSidebar extends Vue {
-  public sidebar: boolean = true;
-  @Prop({ required: true }) profileNavOptions!: any;
-  @Prop({ required: true }) user!: any;
-  changeView(choice: any) {
-    this.sidebar = false;
-    this.$emit("view-change", choice);
+  data() {
+    return {
+      pages: [
+        {
+          name: "Dane użytkownika",
+          link: "/uzytkownik/dane",
+          icon: "mdi-account-box",
+        },
+        {
+          name: "Zmiana hasła",
+          link: "/uzytkownik/haslo",
+          icon: "mdi-shield",
+        },
+        {
+          name: "Recenzje użytkownika",
+          link: "/uzytkownik/recenzje",
+          icon: "mdi-file-document",
+        },
+        {
+          name: "Listy użytkownika",
+          link: "/uzytkownik/listy",
+          icon: "mdi-playlist-play",
+        },
+        {
+          name: "Administrator",
+          link: "/uzytkownik/administrator",
+          icon: "mdi-account-supervisor",
+        },
+      ],
+      user: {
+        nickName: "jankowalski123",
+        avatar:
+          "https://play-lh.googleusercontent.com/5LIMaa7WTNy34bzdFhBETa2MRj7mFJZWb8gCn_uyxQkUvFx_uOFCeQjcK16c6WpBA3E=s180-rw",
+      },
+    };
   }
-  get sidebarShown(): boolean {
-    return this.sidebar;
-  }
-  set sidebarShown(newValue) {
-    this.sidebar = newValue;
-  }
-  calcTopbarPos(): number {
+
+  get TopbarPos(): number {
     return this.$vuetify.application.top / 2;
+  }
+
+  get sidebarShown(): boolean {
+    return this.$store.state.userProfile.sidebarShown;
+  }
+
+  set sidebarShown(newVal: boolean) {
+    this.$store.commit("setProfileSidebarShown", newVal);
   }
 }
 </script>
-
-<template>
-  <v-container>
-    <v-app-bar app clipped-left :style="{ top: calcTopbarPos() + 'px' }">
-      <v-app-bar-nav-icon @click="sidebar = !sidebar"></v-app-bar-nav-icon>
-      <v-spacer />
-      <v-toolbar-title class="text-h6">Profil użytkownika</v-toolbar-title>
-      <v-spacer />
-    </v-app-bar>
-    <v-navigation-drawer
-      app
-      temporary
-      hide-overlay
-      left
-      v-model="sidebarShown"
-      :style="{ top: $vuetify.application.top + 'px', zIndex: 4 }"
-      clipped
-    >
-      <v-list>
-        <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title>
-              <v-list-item-avatar>
-                <v-img :src="user.avatar"></v-img>
-              </v-list-item-avatar>
-              {{ user.nickName }}
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-divider></v-divider>
-        <v-list-item
-          v-for="item in profileNavOptions"
-          :key="item.title"
-          link
-          @click="changeView(item)"
-        >
-          <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  </v-container>
-</template>
--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,169 +1,45 @@
-<script lang="ts">
-import Vue from "vue";
-import { Component } from "vue-property-decorator";
-
-import ProfileSidebar from "@/components/profilePage/profileSidebar.vue";
-import ProfileInfoForm from "@/components/profilePage/ProfileInfoForm.vue";
-import ChangePasswordForm from "@/components/profilePage/ChangePasswordForm.vue";
-import ReviewList from "@/components/profilePage/ReviewList.vue";
-import UserLists from "@/components/profilePage/UserLists.vue";
-
-@Component({
-  components: {
-    ProfileSidebar,
-    ProfileInfoForm,
-    ChangePasswordForm,
-    ReviewList,
-    UserLists,
-  },
-})
-export default class UserProfile extends Vue {
-  // ----------------------------------------------
-  // example data
-  public user: object = {
-    firstName: "Jan",
-    lastName: "Kowalski",
-    nickName: "jankowalski123",
-    email: "jankowalski@gmail.com",
-    avatar:
-      "https://i0.wp.com/www.iskmogul.com/wp-content/uploads/2020/05/super-mario-64.jpg?resize=1140%2C694&ssl=1",
-  };
-  public userReviews: Array<any> = [
-    {
-      movieTitle: "Tytul1",
-      date: "data1",
-      type: "pozytywna",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce volutpat hendrerit cursus. Morbi id mauris vehicula, commodo risus at, molestie ante. Curabitur gravida tellus at dui ultrices, eu pulvinar eros euismod. Nam laoreet orci sit amet cursus sollicitudin. Vestibulum vitae tellus accumsan, vulputate dolor vel, convallis augue. In mollis massa ac posuere dapibus. Morbi pharetra ante ut augue varius, et convallis risus pharetra. Vivamus ut nisl efficitur, tempus ante nec, luctus mi. Maecenas euismod malesuada arcu, vitae accumsan arcu pretium id. Proin sit amet turpis ex. Maecenas pharetra vehicula arcu quis dapibus.Integer leo purus, tincidunt sed vehicula id, hendrerit vel nisl. Sed laoreet at mauris non ullamcorper. Vestibulum sed scelerisque tellus, quis ultricies leo. Aenean tristique tincidunt lectus, vitae pretium nisi aliquet non. Quisque tempus scelerisque est nec pretium. Pellentesque viverra hendrerit dignissim. Vestibulum elit erat, porttitor eu tincidunt eu, auctor et orci. Integer ac tempor magna. Mauris imperdiet sed libero sit amet hendrerit. Sed dignissim porttitor ultricies. Morbi commodo, est a faucibus malesuada, libero tortor pharetra augue, at tempor odio dui at nunc. Nullam ut lacus volutpat nunc sodales commodo.Donec tincidunt tempus dolor, sed pretium mauris aliquam vitae. Curabitur tristique ex nec quam ullamcorper, sed dictum libero semper. Mauris ac neque finibus, aliquet diam quis, ullamcorper diam. Duis malesuada efficitur ligula at dignissim. Morbi a tortor dignissim, rutrum tortor non, scelerisque ante. Phasellus pulvinar dapibus scelerisque. Vivamus tincidunt sed ligula non volutpat. Proin tristique a mi sit amet ornare. Proin at mi lacus.",
-    },
-    {
-      movieTitle: "Tytul2",
-      date: "data2",
-      type: "negatywna",
-      content: "tresc2",
-    },
-    {
-      movieTitle: "Tytul3",
-      date: "data3",
-      type: "pozytywna",
-      content: "tresc3",
-    },
-  ];
-  public userLists: Array<any> = [
-    {
-      name: "Ulubione",
-      movies: [
-        { title: "Star Wars", id: 1 },
-        { title: "Forest Gump", id: 2 },
-        { title: "Die Hard", id: 3 },
-      ],
-      showDialog: false,
-      id: 1,
-    },
-    {
-      name: "Badziej ulubione",
-      movies: [
-        { title: "Spider-man", id: 4 },
-        { title: "Avengers", id: 5 },
-        { title: "Batman", id: 6 },
-      ],
-      showDialog: false,
-      id: 2,
-    },
-    {
-      name: "Jeszcze bardziej ulubione",
-      movies: [
-        { title: "Muminki", id: 7 },
-        { title: "Krecik", id: 8 },
-        { title: "Reksio", id: 9 },
-      ],
-      showDialog: false,
-      id: 3,
-    },
-  ];
-
-  // ----------------------------------------------
-  public nextListId = 3;
-      show: false,
-    },
-    { title: "Zmiana hasła", icon: "mdi-shield", show: false },
-    {
-      title: "Recenzje użytkownika",
-      icon: "mdi-file-document",
-      show: false,
-    },
-    {
-      title: "Listy użytkownika",
-      icon: "mdi-playlist-play",
-      show: false,
-    },
-  ];
-
-  changeView(choice: any): void {
-    this.navOptions.forEach((item) => {
-      item.show = false;
-    });
-    choice.show = true;
-  }
-
-  createList(listName: string): void {
-    this.userLists.push({
-      name: listName,
-      movies: [],
-      id: ++this.nextListId,
-      showDialog: false,
-    });
-  }
-
-  renameList(listId: number, listName: string): void {
-    var index: number = this.userLists.findIndex((list) => list.id === listId);
-    this.userLists[index].name = listName;
-  }
-
-  removeList(id: number): void {
-    this.userLists = this.userLists.filter((list) => {
-      return list.id !== id;
-    });
-  }
-
-  removeMovie(listId: number, movieId: number): void {
-    var index: number = this.userLists.findIndex((list) => list.id === listId);
-    this.userLists[index].movies = this.userLists[index].movies.filter(
-      (movie: Object) => {
-        return movie.id !== movieId;
-      }
-    );
-  }
-}
-</script>
-
 <template>
   <v-container>
+    <v-app-bar app clipped-left :style="{ top: TopbarPos + 'px' }">
+      <v-app-bar-nav-icon
+        @click="sidebarShown = !sidebarShown"
+      ></v-app-bar-nav-icon>
+      <v-spacer />
+      <v-toolbar-title class="text-h6">Profil użytkownika</v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
     <v-row>
       <v-col>
-        <ProfileSidebar
-          :user="user"
-          :profileNavOptions="navOptions"
-          @view-change="changeView"
-        />
+        <ProfileSidebar />
       </v-col>
     </v-row>
     <v-col><v-spacer></v-spacer></v-col>
     <v-col></v-col>
-    <v-col v-if="navOptions[0].show"> <ProfileInfoForm :user="user" /></v-col>
-    <v-col v-else-if="navOptions[1].show"> <ChangePasswordForm /></v-col>
-    <v-col v-else-if="navOptions[2].show">
-      <ReviewList :reviews="userReviews"
-    /></v-col>
-    <v-col v-else-if="navOptions[3].show">
-      <UserLists
-        @remove-list="removeList"
-        @remove-movie="removeMovie"
-        @create-list="createList"
-        @rename-list="renameList"
-        :userLists="userLists"
-    /></v-col>
+    <router-view></router-view>
   </v-container>
 </template>
 
-<style scoped></style>
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import ProfileSidebar from "@/components/profilePage/profileSidebar.vue";
+
+@Component({
+  components: {
+    ProfileSidebar,
+  },
+})
+export default class UserProfile extends Vue {
+  get TopbarPos(): number {
+    return this.$vuetify.application.top / 2;
+  }
+
+  get sidebarShown(): boolean {
+    return this.$store.state.userProfile.sidebarShown;
+  }
+
+  set sidebarShown(newVal: boolean) {
+    this.$store.commit("setProfileSidebarShown", newVal);
+  }
+}
+</script>
