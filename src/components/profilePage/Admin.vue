74face11ebf93e3c1105bb9f0a2c966ffe185ca1
MaciekFerenc <maciekferenc1@gmail.com>
2022-06-04 18:48:07 +0200

    Add admin panel
    Admin can add movies, import movies, add people

--- src/components/profilePage/Admin.vue
+++ src/components/profilePage/Admin.vue
@@ -1,11 +1,57 @@
 <template>
-  <v-card> Administrator </v-card>
+  <v-container>
+    <v-list>
+      <v-subheader class="text-h6">Panel administratora</v-subheader>
+      <v-list-item v-for="(page, i) in pages" :key="i" link :to="page.link">
+        <v-list-item-content>
+          <v-list-item-title> {{ page.name }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+    <v-spacer></v-spacer>
+    <v-divider class="spacer"></v-divider>
+    <router-view></router-view>
+  </v-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
+import AddMovieForm from "@/components/profilePage/AddMovieForm.vue";
+import AddPersonForm from "@/components/profilePage/AddPersonForm.vue";
+import ImportMovieForm from "@/components/profilePage/ImportMovieForm.vue";
 
-@Component
-export default class Admin extends Vue {}
+@Component({
+  components: {
+    AddMovieForm,
+    AddPersonForm,
+    ImportMovieForm,
+  },
+})
+export default class Admin extends Vue {
+  data() {
+    return {
+      pages: [
+        {
+          name: "Dodaj film",
+          link: "/uzytkownik/administrator/dodawanie-filmu",
+        },
+        {
+          name: "Importuj film",
+          link: "/uzytkownik/administrator/import-filmu",
+        },
+        {
+          name: "Dodaj osobę",
+          link: "/uzytkownik/administrator/dodawanie-osoby",
+        },
+      ],
+    };
+  }
+}
 </script>
+
+<style scoped>
+.spacer {
+  padding: 15px;
+}
+</style>
