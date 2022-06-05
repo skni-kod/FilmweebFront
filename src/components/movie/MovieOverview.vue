abde7f50696335032028ba59ac668c8f2a6225dc
Hubert <102256135+hfej@users.noreply.github.com>
2022-06-05 14:53:57 +0200

    Added external site links

--- src/components/movie/MovieOverview.vue
+++ src/components/movie/MovieOverview.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="wrapper-blk">
     <div class="poster">
-      <v-img
-          lazy-src="../../assets/logo.svg"
-          src="../../assets/logo.svg"
-      />
+      <v-img lazy-src="../../assets/logo.svg" src="../../assets/logo.svg" />
     </div>
     <div class="plot">
-      Sed iaculis elit sed leo accumsan consectetur. Quisque sagittis libero id sem lacinia egestas. Phasellus egestas
-      facilisis diam non tristique.
+      Sed iaculis elit sed leo accumsan consectetur. Quisque sagittis libero id
+      sem lacinia egestas. Phasellus egestas facilisis diam non tristique.
     </div>
     <ul class="tag-ul">
       <li class="detail-li">
@@ -27,25 +24,129 @@
       </li>
       <li class="detail-li">
         <div class="tag tags--text">Czas trwania:</div>
-        <div class="value" tabindex="0">{{ durationFormat(movieData.czas_trwania) }}</div>
+        <div class="value" tabindex="0">
+          {{ durationFormat(movieData.czas_trwania) }}
+        </div>
+      </li>
+      <li class="buttons">
+        <div class="tag tags--text">Odnośniki:</div>
+        <v-row>
+          <v-col v-for="(reference, i) in references" :key="i" cols="auto">
+            <a :href="`${reference.link}`" target="_blank">
+              <v-img
+                v-if="reference.type.charAt(0) === 'f'"
+                class="mt-3"
+                contain
+                position="left"
+                rounded
+                height="35px"
+                width="35px"
+                :src="`https://i.imgur.com/xWvDSIF.png`"
+              >
+                <template v-slot:placeholder>
+                  <v-row
+                    class="fill-height ma-0"
+                    align="center"
+                    justify="center"
+                  >
+                    <v-progress-circular
+                      indeterminate
+                      color="grey lighten-5"
+                    ></v-progress-circular>
+                  </v-row>
+                </template>
+              </v-img>
+
+              <v-img
+                v-if="reference.type.charAt(0) === 'r'"
+                class="mt-3"
+                contain
+                position="left"
+                rounded
+                height="35px"
+                width="35px"
+                :src="`https://i.imgur.com/N4S2F5y.png`"
+              >
+                <template v-slot:placeholder>
+                  <v-row
+                    class="fill-height ma-0"
+                    align="center"
+                    justify="center"
+                  >
+                    <v-progress-circular
+                      indeterminate
+                      color="grey lighten-5"
+                    ></v-progress-circular>
+                  </v-row>
+                </template>
+              </v-img>
+
+              <v-img
+                v-if="reference.type.charAt(0) === 'i'"
+                class="mt-3"
+                contain
+                position="left"
+                rounded
+                height="35px"
+                width="35px"
+                :src="`https://i.imgur.com/CiCn2Vl.png`"
+              >
+                <template v-slot:placeholder>
+                  <v-row
+                    class="fill-height ma-0"
+                    align="center"
+                    justify="center"
+                  >
+                    <v-progress-circular
+                      indeterminate
+                      color="grey lighten-5"
+                    ></v-progress-circular>
+                  </v-row>
+                </template>
+              </v-img>
+            </a>
+          </v-col>
+        </v-row>
       </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
 
 @Component({})
 export default class MovieOverview extends Vue {
-  @Prop({required: true}) movieData: any;
+  @Prop({ required: true }) movieData: any;
 
   private durationFormat(duration: number) {
     let hours = Math.trunc(duration / 60);
-    let minutes = (duration - hours * 60);
+    let minutes = duration - hours * 60;
     return `${hours}h ${minutes}min`;
-  };
+  }
+
+  data() {
+    return {
+      references: [
+        {
+          id: "1",
+          type: "filmweb",
+          link: "https://www.filmweb.pl/film/Fantastyczne+zwierz%C4%99ta%3A+Tajemnice+Dumbledore%27a-2022-731997",
+        },
+        {
+          id: "2",
+          type: "rotten",
+          link: "https://www.rottentomatoes.com/m/top_gun_maverick",
+        },
+        {
+          id: "3",
+          type: "imdb",
+          link: "https://www.imdb.com/title/tt1877830/",
+        },
+      ],
+    };
+  }
 }
 </script>
 
@@ -84,5 +185,10 @@ export default class MovieOverview extends Vue {
 
 .movie-overview .poster > div {
   width: 100%;
+}
+
+a {
+  display: block;
+  width: 35px;
 }
 </style>
