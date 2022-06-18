b37df460c70b19cf5701d79e8db5f00922de80cf
Krystian Kielbasa <krystian.kielbasa@hotmail.com>
2022-06-18 05:46:53 +0200

    Add genres & basic btn of Sidetools

--- src/components/actor/ActorSidetools.vue
+++ src/components/actor/ActorSidetools.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <div>
-      Sidetool section or something like that.
+  <div class="wrapper-blk">
+    <div class="actor-tools" v-for="(btn, i) in actorTools" :key="i">
+      <v-btn v-if="!btn.admin || btn.admin === adminState" @click="btn.click" >
+        <v-icon>{{ btn.icon }}</v-icon>
+        <span class="white--text"> {{ btn.text }}</span>
+      </v-btn>
     </div>
   </div>
 </template>
@@ -11,10 +14,55 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 
 @Component({})
-export default class MovieSidetools extends Vue {
+export default class ActorSidetools extends Vue {
+
+  data() {
+    return {
+      actorTools: [
+        {
+          text: "Edytuj aktora",
+          click: this.editActor,
+          icon: "mdi-playlist-edit",
+          admin: true,
+        },
+        {
+          text: "Usuń aktora",
+          click: this.removeActor,
+          icon: "mdi-playlist-remove",
+          admin: true,
+        }
+      ]
+    }
+  }
+
+  private editActor() {
+
+  }
+
+  private removeActor() {
+
+  }
+
+  get userState() {
+    return this.$store.getters.isAuthenticated;
+  }
+
+  get adminState() {
+    return this.$store.getters.isAdmin;
+  }
+
 }
 </script>
 
 <style lang="scss" scoped>
+.actor-tools {
+  display: flex;
+  flex-wrap: wrap;
+}
 
+.actor-tools > button {
+  width: 100%;
+  margin: 5px 0;
+  padding: 24px 16px !important;
+}
 </style>
--- src/components/movie/MovieSidetools.vue
+++ src/components/movie/MovieSidetools.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <div>
-      Sidetools section or something like that.
+  <div class="wrapper-blk">
+    <div class="movie-tools" v-for="(btn, i) in movieTools" :key="i">
+      <v-btn v-if="!btn.admin || btn.admin === adminState" @click="btn.click" >
+        <v-icon>{{ btn.icon }}</v-icon>
+        <span class="white--text"> {{ btn.text }}</span>
+      </v-btn>
     </div>
   </div>
 </template>
@@ -12,9 +15,64 @@ import {Component} from 'vue-property-decorator';
 
 @Component({})
 export default class MovieSidetools extends Vue {
+
+  data() {
+    return {
+      movieTools: [
+        {
+          text: "Dodaj do listy",
+          click: this.addToList,
+          icon: "mdi-playlist-plus",
+          admin: false,
+        },
+        {
+          text: "Edytuj film",
+          click: this.editMovie,
+          icon: "mdi-playlist-edit",
+          admin: true,
+        },
+        {
+          text: "Usuń film",
+          click: this.removeMovie,
+          icon: "mdi-playlist-remove",
+          admin: true,
+        }
+      ]
+    }
+  }
+
+  private addToList() {
+
+  }
+
+  private editMovie() {
+
+  }
+
+  private removeMovie() {
+
+  }
+
+  get userState() {
+    return this.$store.getters.isAuthenticated;
+  }
+
+  get adminState() {
+    return this.$store.getters.isAdmin;
+  }
+
 }
 </script>
 
 <style lang="scss" scoped>
+.movie-tools {
+  display: flex;
+  flex-wrap: wrap;
+}
 
+.movie-tools > button {
+  width: 100%;
+  margin: 5px 0;
+  padding: 24px 16px !important;
+}
 </style>
